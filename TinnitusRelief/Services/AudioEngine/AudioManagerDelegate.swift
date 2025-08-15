c3d1f1516ade201f89cf0ecbce0e869dc8179e29
Midnight <midnight@Midnightui-MacBookAir.local>
2025-08-15 20:41:24 +0900

    clean up

--- TinnitusRelief/Services/AudioEngine/AudioManagerDelegate.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-protocol AudioManagerDelegate: AnyObject {
-    func audioManagerDidUpdateFrequency(_ frequency: Float)
-    func audioManagerDidUpdateVolume(_ volume: Float) 
-    func audioManagerDidUpdatePlayingState(_ isPlaying: Bool)
-}
